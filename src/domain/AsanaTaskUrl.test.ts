1cfb5339fe8ec16134b207ba5ddb244a71535c1e
H37kouya <h37kouya@gmail.com>
2021-03-06 15:35:12 +0900

    feat テストを通した

--- src/domain/AsanaTaskUrl.test.ts
+++ src/domain/AsanaTaskUrl.test.ts
@@ -12,16 +12,50 @@ describe("AsanaTaskUrl", () => {
       {
         arg: ASANA_URL('/foo/bar'),
         expected: ASANA_URL('/foo/bar')
+      },
+      {
+        arg: ASANA_URL('/foo/bar/'),
+        expected: ASANA_URL('/foo/bar')
       }
     ]
 
     testCases.forEach(testCase => {
       it(`arg=${testCase.arg}, expected=${testCase.expected}`, () => {
         // WHEN
-        const actual = new AsanaTaskUrl(testCase.arg)
+        const actual = AsanaTaskUrl.of(testCase.arg)
 
+        // THEN
         expect(actual).toBeInstanceOf(AsanaTaskUrl)
         expect(actual.get()).toBe(testCase.expected)
+      })
+    })
+  })
+
+  describe("taskGidを取得できる", () => {
+    type TestCase = {
+      arg: string;
+      expected: string;
+    };
+
+    const testCases: TestCase[] = [
+      {
+        arg: ASANA_URL('/foo/bar'),
+        expected: 'bar'
+      },
+      {
+        arg: ASANA_URL('/foo/bar/'),
+        expected: 'bar'
+      }
+    ]
+
+    testCases.forEach(testCase => {
+      it(`arg=${testCase.arg}, expected=${testCase.expected}`, () => {
+        // WHEN
+        const actual = AsanaTaskUrl.of(testCase.arg)
+
+        // THEN
+        expect(actual).toBeInstanceOf(AsanaTaskUrl)
+        expect(actual.taskGid()).toBe(testCase.expected)
       })
     })
   })
--- src/domain/AsanaTaskUrl.ts
+++ src/domain/AsanaTaskUrl.ts
@@ -1,26 +1,43 @@
 import { URL } from "url";
 import { ASANA_BASE_URL } from "../const";
 import { getLastPath } from "../url";
-import { ValueObject } from "./ValueObject";
 import { InvalidArgumentError } from './InvalidArgumentError'
 
 declare const Unique: unique symbol;
 
-export class AsanaTaskUrl extends ValueObject<string> {
+export class AsanaTaskUrl {
   // @ts-ignore
   private [Unique]: void;
 
-  constructor(protected val: string) {
-    super(val)
+  constructor(protected value: string) {}
 
-    const newUrl = new URL(val)
+  public get() {
+    return this.value;
+  }
+
+  public eq(
+    valueObject: AsanaTaskUrl
+  ): boolean {
+    return valueObject.get() === this.value;
+  }
+
+  public static of = function(
+    this: new (value: string) => AsanaTaskUrl,
+    value: string
+  ) {
+    // 末尾のスラッシュを削除
+    const formatUrl = value.replace(/\/$/, '')
+
+    const newUrl = new URL(formatUrl)
 
     if (`${newUrl.origin}/0` !== ASANA_BASE_URL)
       throw new InvalidArgumentError(
-        `有効なAsanaのタスクURLではありません。ASANA_BASE_URLが異なります。 origin=${newUrl.origin}/0, val=${val}`
+        `有効なAsanaのタスクURLではありません。ASANA_BASE_URLが異なります。 origin=${newUrl.origin}/0, value=${value}`
       )
-    if (newUrl.pathname.split('/').length !== 4) throw new InvalidArgumentError(`有効なAsanaのタスクURLではありません。パスの数が異なります。val=${val}`)
-  }
+    if (newUrl.pathname.split('/').length !== 4) throw new InvalidArgumentError(`有効なAsanaのタスクURLではありません。パスの数が異なります。value=${value}`)
+
+    return new this(formatUrl);
+  };
 
   public taskGid() {
     return getLastPath(this.value)
